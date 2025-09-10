d1001904fa895716530884b24556cc8cff7552d0
Shaban <155167978+Ahmedshaban123r@users.noreply.github.com>
2025-09-11 02:24:25 +0300

    Update Motor.c

--- final project/final project/Motor.c
+++ final project/final project/Motor.c
@@ -12,14 +12,14 @@ void motor_init()
 	//timer1 configuration for fast pwm
 	TIMER_Config_t t1_cfg={
 		.id =TIMER_ID_1,
-		.mode = TIMER_MODE_FAST_PWM ,	  // Fast PWM mode
+		.mode = TIMER_MODE_FAST_PWM_10BIT,	  // Fast PWM mode
 		.clock_sel= TIMER01_CLK_8,		 // Prescaler = 8 (~2kHz PWM)
 		.oc_mode_A= TIMER_OC_CLEAR,		  // Non-inverting on OC1A (PD5)
 		.oc_mode_B= TIMER_OC_CLEAR,		// Non-inverting on OC1B (PD4)
 		.tcnt_init=0,
 		.ocrA_init=0,
 		.ocrB_init=0,
-		.top_icr1=0,
+		.top_icr1=1023,
 		.int_ovf_enable=0,
 		.int_ocA_enable=0,
 		.int_ocB_enable=0,
@@ -38,9 +38,14 @@ void motor_init()
 }
 
 void motor_set_speed(uint8_t left_spd, uint8_t right_spd)
-{
-	TIMER_setDutyRaw(TIMER_ID_1,TIMER_CH_A,right_spd);
-	TIMER_setDutyRaw(TIMER_ID_1,TIMER_CH_B,left_spd);
+{	 // Ensure speeds are within valid range (0-255)
+	left_spd = (left_spd > 255) ? 255 : left_spd;
+	right_spd = (right_spd > 255) ? 255 : right_spd;
+	 // Convert 8-bit (0-255) to 10-bit (0-1023)
+	 uint16_t left_10bit = (left_spd * 1023UL) / 255;
+	 uint16_t right_10bit = (right_spd * 1023UL) / 255;
+	TIMER_setDutyRaw(TIMER_ID_1,TIMER_CH_A, right_10bit );
+	TIMER_setDutyRaw(TIMER_ID_1,TIMER_CH_B,left_10bit);
 }
 
 void motor_control(Motor_Directions dir, uint8_t left_spd, uint8_t right_spd)
@@ -48,9 +53,6 @@ void motor_control(Motor_Directions dir, uint8_t left_spd, uint8_t right_spd)
 	 /* apply speeds first (so transitions are smoother) */
 	 motor_set_speed(left_spd, right_spd);
 
-	 /* Now set direction pins according to bit-mask in dir (nibble mapping: bit0..3 => IN1..IN4) */
-	 if (dir & 0x01) PORTD |=  (1 << IN1_PIN); else PORTD &= ~(1 << IN1_PIN); // IN1
-	 if (dir & 0x02) PORTD |=  (1 << IN2_PIN); else PORTD &= ~(1 << IN2_PIN); // IN2
-	 if (dir & 0x04) PORTD |=  (1 << IN3_PIN); else PORTD &= ~(1 << IN3_PIN); // IN3
-	 if (dir & 0x08) PORTD |=  (1 << IN4_PIN); else PORTD &= ~(1 << IN4_PIN); // IN4
+	 /* Set all direction pins at once using the direction mask */
+	 PORTD = (PORTD & 0x03) | (dir & 0xFC); // Preserve PD0-PD1, set PD2-PD7 from direction
 }
